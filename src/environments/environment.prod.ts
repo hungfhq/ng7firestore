08775cb6d18316da047725e94365f20dafabeafc
hung hq <hungfhq@gmail.com>
2019-04-07 00:39:32 +0700

    before push to github

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyD9WF-R_RqGuUZKW-vdD1f9D6_uTHrnaRs',
+    authDomain: 'ng7firestore.firebaseapp.com',
+    databaseURL: 'https://ng7firestore.firebaseio.com',
+    projectId: 'ng7firestore',
+    storageBucket: 'ng7firestore.appspot.com',
+    messagingSenderId: '91532790238'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyD9WF-R_RqGuUZKW-vdD1f9D6_uTHrnaRs',
+    authDomain: 'ng7firestore.firebaseapp.com',
+    databaseURL: 'https://ng7firestore.firebaseio.com',
+    projectId: 'ng7firestore',
+    storageBucket: 'ng7firestore.appspot.com',
+    messagingSenderId: '91532790238'
+  }
 };
 
 /*
